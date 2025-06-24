9ddcd26120be1424077eeff2558e107741c33d0c
vsafedot <siddharthnain3@gmail.com>
2025-06-24 18:43:53 +0530

    cocktail section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/hero.jsx";
+import Cocktails from "./components/cocktails.jsx";
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
@@ -11,7 +12,7 @@ const App = () => {
             <main>
                 <Navbar />
                 <Hero/>
-                <div className="h-dvh bg-black"></div>
+                <Cocktails />
             </main>
         </>
     );
